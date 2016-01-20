12192f5317bc3526bd356241d26e0fe295605bb4
NoneOfMaster <rcraigt@gmail.com>
2016-01-20 12:17:33 -0800

    I love it when a lab comes together

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user
+  has_many :post_tags ###NEVER FORGET THIS LINE IS NECESSARY FOR THE NEXT!!!
+  has_many :tags, through: :post_tags
+
+  validates :name, :content, presence: true
+
 end
